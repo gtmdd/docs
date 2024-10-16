9fc25d3fcd43e3e37de8f511ed889990e3a4bbf8
Erik <andrew@monkemods.xyz>
2024-10-16 09:12:26 -0500

    Mod init + licensing

---
+++ code_samples/plain_mod_initialization.cs
@@ -0,0 +1,14 @@
+/*
+Code sample from "Program Structure": program_structure.md
+(C) Copyright 2024 GTMDD Authors. GNU LGPL V3
+*/
+
+using System;
+using UnityEngine;
+using BepInEx;
+
+namespace Mod {
+	public class ModInit {
+		[BepInPlugin(“com.myname.gorillatag.mymod”, “My mod name”, “v1.0.0”)]
+	}
+}
